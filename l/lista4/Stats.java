90b53527fe238df747e98eac841aaa53a5683c14
krymeer <krymeer@gmail.com>
2017-04-28 07:18:52 +0200

    List 4: added necessary statistics

--- l/lista4/Stats.java
+++ l/lista4/Stats.java
@@ -29,19 +29,47 @@ public class Stats {
     return max;
   }
 
+  public static void createBalancedTree(Tree tree, int[] arr, int start, int end) {
+    if (end-start == 0) {
+      tree.insert(arr[end]);
+    } else if (end-start == 1) {
+      tree.insert(arr[start]);
+      tree.insert(arr[end]);
+    } else {
+      int m = (start+end)/2;
+      tree.insert(arr[m]);
+      createBalancedTree(tree, arr, start, m-1);
+      createBalancedTree(tree, arr, m+1, end);
+    }
+  }
+
   public static void main(String[] args) {
     int[] size = {25, 50, 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000}, stats = new int[50], height = new int[50];
     int minComp, maxComp, avgComp, minHeight, avgHeight, maxHeight;
     Random random = new Random();
+
+    if (args.length == 0) {
+      System.err.println("\nużycie:\n\tjava Stats typDanychWejściowych badaneStatystyki typWartości\ngdzie:\n\ttypDanychWejściowych:\n\t  drzewo zbalansowane [bal], ciąg posortowany rosnąco [asc] lub ciąg losowo wybranych elementów [rand]\n\tbadaneStatystyki:\n\t  liczba porównań [comparisons] lub wysokość utworzonych drzew [height]\n\ttypWartości:\n\t  wartości minimalne [min], średnie [avg] bądź maksymalne [max]\n");
+      return;
+    }
+
     for (int i = 0; i < 14; i++) {
       for (int k = 0; k < 50; k++) {
         Tree tree = new Tree();
-        for (int j = 0; j < size[i]; j++) {
-          if (args[0].equals("asc")) {
-            tree.insert(j);
-          } else {
-            tree.insert(random.nextInt(10000));
+        if (args[0].equals("asc") || args[0].equals("rand")) {
+          for (int j = 0; j < size[i]; j++) {
+            if (args[0].equals("asc")) {
+              tree.insert(j);
+            } else {
+              tree.insert(random.nextInt(10000));
+            }
           }
+        } else {
+          int arr[] = new int[size[i]];
+          for (int j = 0; j < size[i]; j++) {
+            arr[j] = j;
+          }
+          createBalancedTree(tree, arr, 0, size[i]-1);
         }
         tree.numberOfComparisons = 0;
         tree.find(random.nextInt(10000), false);
